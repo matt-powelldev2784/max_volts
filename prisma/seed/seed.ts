f3c812b59084cea8a0249744ae3458738ca546b8
Matthew Powell <matt.powell2784@gmail.com>
2023-08-08 13:32:42 +0100

    fix: inititalize husky git hooks

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -2,21 +2,19 @@ import { prisma } from '../db/client'
 import { storeItemSeeds } from './storeItems'
 
 async function main(): Promise<void> {
-//   await prisma.storeItem.deleteMany()
-
-//   const storeItems = await prisma.storeItem.createMany({
-//     data: storeItemSeeds.map((product) => ({
-//       name: product.name,
-//       image: product.image,
-//       brand: product.brand,
-//       description: product.description,
-//       price: product.price,
-//       category: product.category,
-//     })),
-//   })
-
-//   console.log({ storeItems })
-// }
+  //   await prisma.storeItem.deleteMany()
+  //   const storeItems = await prisma.storeItem.createMany({
+  //     data: storeItemSeeds.map((product) => ({
+  //       name: product.name,
+  //       image: product.image,
+  //       brand: product.brand,
+  //       description: product.description,
+  //       price: product.price,
+  //       category: product.category,
+  //     })),
+  //   })
+  //   console.log({ storeItems })
+}
 
 main()
   .then(async () => {
