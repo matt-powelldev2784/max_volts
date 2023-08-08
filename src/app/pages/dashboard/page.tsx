9bc649e5db420dc25cbbb116739227537cd0386e
Matthew Powell <matt.powell2784@gmail.com>
2023-08-08 13:31:54 +0100

    fix: inititalize husky git hooks

--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,11 +1,6 @@
 import { NavBar } from '@/app/components'
-import { checkUserIsAdmin } from '@/app/lib/getLoggedInUser'
-import { redirect } from 'next/navigation'
 
 export default async function DashboardPage() {
-  const isAdmin = await checkUserIsAdmin()
-  if (!isAdmin) redirect('/')
-
   return (
     <main className="min-h-screen min-w-screen">
       <NavBar />
