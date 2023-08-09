3e6c8e01c48acc7e1828bcbd505ab7215ae1f1a1
Matthew Powell <matt.powell2784@gmail.com>
2023-08-09 20:16:45 +0100

    feat: add invoice row component

--- src/app/components/invoice/components/AddInvoiceRow.tsx
+++ src/app/components/invoice/components/AddInvoiceRow.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react'
 import { SelectField } from './SelectField'
-import { InputField } from './InputField'
 import { useFormik } from 'formik'
 import { ClientName } from '@/types/clientName'
 import { Button } from '@/ui/button/button'
@@ -10,30 +9,32 @@ import * as Yup from 'yup'
 import { apiCall } from '@/lib/apiCall'
 
 export const AddInvoiceRow = () => {
-  const [clients, setClients] = useState<ClientName[]>([])
   const [products, setProducts] = useState<ClientName[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  console.log('clients', clients)
   console.log('products', products)
 
   const formik = useFormik({
     initialValues: {
-      clientId: '',
-      totalAmount: '',
+      productId: '',
     },
     validationSchema: Yup.object({
-      clientId: Yup.string().required('Please input a value client'),
-      totalAmount: Yup.string().required('Please input a invoice total'),
+      productId: Yup.string().required('Please select a product'),
     }),
     onSubmit: async (values) => {
       setIsLoading(true)
-      console.log('values', values)
+
+      const product = products.find(
+        (product) => product.id === values.productId
+      )
+      const invoiceId = sessionStorage.getItem('currentInvoiceId')
+      const invoiceRow = { invoiceId, ...product }
+
 
       try {
         await apiCall({
           httpMethod: 'POST',
-          route: `/api/protected/invoice`,
-          body: values,
+          route: `/api/protected/invoice-row/`,
+          body: invoiceRow,
         })
       } catch (error: any) {
         console.log('error', error.message)
@@ -44,29 +45,14 @@ export const AddInvoiceRow = () => {
   })
 
   useEffect(() => {
-    const getClientsData = async () => {
-      const clientsData: ClientName[] = await apiCall({
-        route: `/api/protected/client/clientname`,
-      })
-      setClients(clientsData)
-    }
     const getProductsData = async () => {
       const productsData: ClientName[] = await apiCall({
         route: `/api/protected/product`,
       })
       setProducts(productsData)
     }
-    getClientsData()
     getProductsData()
   }, [])
-
-  const clientSelectOptionsJsx = clients.map((client) => {
-    return (
-      <option key={client.id} value={client.id}>
-        {`${client.name} @ ${client.companyName}`}
-      </option>
-    )
-  })
 
   const productSelectOptionsJsx = products.map((product) => {
     return (
@@ -83,31 +69,19 @@ export const AddInvoiceRow = () => {
     >
       <p>Add Invoice Row</p>
 
-      <SelectField formik={formik} htmlFor="clientId" labelText="Select Client">
-        <option value="">Select a client</option>
-        {clientSelectOptionsJsx}
-      </SelectField>
-
       <SelectField
         formik={formik}
-        htmlFor="selectProduct"
-        labelText="Select Client"
+        htmlFor="productId"
+        labelText="Select Product"
       >
         <option value="">Select a product</option>
         {productSelectOptionsJsx}
       </SelectField>
 
-      <InputField
-        formik={formik}
-        htmlFor="totalAmount"
-        labelText="Invoice Total"
-        inputType="text"
-      />
-
       <Button
         type="submit"
         optionalClasses="w-full bg-red-500 my-2"
-        buttonText="Upload Image"
+        buttonText="Add Invoice Row"
         disabled={isLoading}
       />
     </form>
