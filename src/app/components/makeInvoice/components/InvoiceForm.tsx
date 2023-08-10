fb4a3838068ae9be0f001d7e145f7f7bfea62f1d
Matthew Powell <matt.powell2784@gmail.com>
2023-08-10 17:53:26 +0100

    refactor: split add proudct and invoice form components

---
+++ src/app/components/makeInvoice/components/InvoiceForm.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { SelectField } from './SelectField'
+import { InputField } from './InputField'
+import { useFormik } from 'formik'
+import { Button } from '@/ui/button/button'
+import * as Yup from 'yup'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxsHooks'
+import { getClients } from '@/redux/slice/clientSlice'
+
+export const InvoiceForm = () => {
+  const dispatch = useAppDispatch()
+  const clients = useAppSelector((state) => state.clientReducer.clients)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    dispatch(getClients())
+  }, [dispatch])
+
+  const formik = useFormik({
+    initialValues: {
+      clientId: '',
+      totalAmount: '0.001',
+    },
+    validationSchema: Yup.object({
+      clientId: Yup.string().required('Please input a value client'),
+      totalAmount: Yup.string().required('Please input a invoice total'),
+    }),
+    onSubmit: async (values) => {
+      setIsLoading(true)
+      console.log('values', values)
+    },
+  })
+
+  const clientSelectOptionsJsx = clients.map((client) => {
+    return (
+      <option key={client.id} value={client.id}>
+        {`${client.name} @ ${client.companyName}`}
+      </option>
+    )
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <p>Create Invoice</p>
+
+      <SelectField formik={formik} htmlFor="clientId" labelText="Select Client">
+        <option value="">Select a client</option>
+        {clientSelectOptionsJsx}
+      </SelectField>
+
+      <InputField
+        formik={formik}
+        htmlFor="totalAmount"
+        labelText="Invoice Total"
+        inputType="text"
+      />
+
+      <Button
+        type="submit"
+        optionalClasses="w-full bg-red-500 my-2"
+        buttonText="Create Invoice"
+        disabled={isLoading}
+      />
+    </form>
+  )
+}
