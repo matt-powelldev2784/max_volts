34a0cb7be0d094be1ef8d895cd24ccf228372445
Matthew Powell <matt.powell2784@gmail.com>
2023-08-10 15:37:04 +0100

    fix: remove v1 invoice logic

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,4 +1,3 @@
 export { IsLoggedIn } from './IsLoggedIn/IsLoggedIn'
-export { Invoice } from './invoice/Invoice'
 export { NavBar } from './navbar/NavBar'
 export { MakeInvoice } from './makeInvoice/MakeInvoice'
