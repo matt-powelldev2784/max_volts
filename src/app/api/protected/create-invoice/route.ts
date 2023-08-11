00ae27c61f020a06df674e882905b129119be6a3
Matthew Powell <matt.powell2784@gmail.com>
2023-08-11 14:28:36 +0100

    feat: add create invoice api route

--- src/app/api/protected/create-invoice/route.ts
+++ src/app/api/protected/create-invoice/route.ts
@@ -2,13 +2,16 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma, authOptions, noSessionResponse } from '@/lib'
 import { getServerSession } from 'next-auth'
 import { badRequestError400 } from '../../badRequestError'
+import { T_Product } from '@/types'
 
 export const POST = async (req: NextRequest, _res: NextResponse) => {
   const session = await getServerSession(authOptions)
   if (!session) return noSessionResponse
 
   const data = await req.json()
-  const { clientId, totalPrice, invoiceRows } = data
+  let { clientId, totalPrice, invoiceRows } = data
+
+  clientId = null
 
   if (!clientId || !totalPrice || invoiceRows.length === 0)
     return badRequestError400
@@ -20,7 +23,24 @@ export const POST = async (req: NextRequest, _res: NextResponse) => {
     },
   })
 
-  
+  console.log('newInvoice', newInvoice)
 
-  return NextResponse.json({ test: 'create ivnoice' }, { status: 201 })
+  await prisma.invoiceRow.createMany({
+    data: invoiceRows.map((invoiceRow: T_Product) => ({
+      invoiceId: newInvoice.id,
+      productId: invoiceRow.id,
+      name: invoiceRow.name,
+      description: invoiceRow.description,
+      price: invoiceRow.sellPrice,
+    })),
+  })
+
+  const activeInvoice = await prisma.invoice.update({
+    where: { id: newInvoice.id },
+    data: {
+      isActive: true,
+    },
+  })
+
+  return NextResponse.json({ activeInvoice }, { status: 201 })
 }
