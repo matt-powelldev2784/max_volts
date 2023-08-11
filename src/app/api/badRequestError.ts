c945b53eef0e542c7f76e3f3c66085afa78b1049
Matthew Powell <matt.powell2784@gmail.com>
2023-08-11 15:05:07 +0100

    refactor: update error repsonse

--- src/app/api/badRequestError.ts
+++ src/app/api/badRequestError.ts
@@ -4,9 +4,7 @@ export const badRequestError400 = NextResponse.json(
   {
     success: false,
     status: 400,
-    errors: [
-      { msg: 'Please provide all required fields in the correct format' },
-    ],
+    message: 'Please provide all required fields in the correct format',
   },
   {
     status: 400,
