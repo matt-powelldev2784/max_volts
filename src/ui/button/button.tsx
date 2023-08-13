0ad34b85dd20bdc72853f933333d6eb56d4a5f05
Matthew Powell <matt.powell2784@gmail.com>
2023-08-13 15:03:19 +0100

    style: tidy invoice styles

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -25,7 +25,7 @@ export const Button = ({
       onClick={onClick}
       onSubmit={onSubmit}
       disabled={disabled}
-      className={`rounded-lg font-semibold outline-none p-2 ${optionalClasses}`}
+      className={`rounded-lg font-semibold outline-none p-2 h-[42.5px] ${optionalClasses}`}
     >
       {buttonText}
     </button>
