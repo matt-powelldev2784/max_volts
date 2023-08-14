803c0e9247b13caed603535f81b573c5a94477aa
Matthew Powell <matt.powell2784@gmail.com>
2023-08-14 15:48:21 +0100

    refactor: remove used var

--- src/app/api/protected/create-invoice/route.ts
+++ src/app/api/protected/create-invoice/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma, authOptions, noSessionResponse } from '@/lib'
 import { getServerSession } from 'next-auth'
 import { badRequestError400 } from '@/lib'
-import { T_InvoiceRow, T_Product } from '@/types'
+import { T_InvoiceRow } from '@/types'
 
 export const POST = async (req: NextRequest, _res: NextResponse) => {
   const session = await getServerSession(authOptions)
