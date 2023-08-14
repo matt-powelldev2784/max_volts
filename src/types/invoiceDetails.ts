44ec5b3b03195dcba5c81fd401bd9e676950faaa
Matthew Powell <matt.powell2784@gmail.com>
2023-08-14 16:01:00 +0100

    refactor: remove old invoice routes

---
+++ src/types/invoiceDetails.ts
@@ -0,0 +1,17 @@
+export interface T_InvoiceRow {
+  id: string
+  name: string
+  description: string
+  buyPrice: number
+  sellPrice: number
+  VAT: number
+  quantity: number
+  totalPrice: number
+  reduxId: string
+}
+
+export interface T_InvoiceDetails {
+  clientId: string
+  totalPrice: number
+  invoiceRows: T_InvoiceRow[]
+}
