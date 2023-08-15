6e4564c2b95896d28bdfc1d3ecb9799e18ba1798
Matthew Powell <matt.powell2784@gmail.com>
2023-08-15 20:44:07 +0100

    feat: add product reducer

--- src/redux/slice/productSlice.ts
+++ src/redux/slice/productSlice.ts
@@ -11,12 +11,14 @@ type T_ProductState = {
   isLoading: boolean
   error: string
   products: T_Product[]
+  currentProduct: T_Product | null
 }
 
 const initialState: T_ProductState = {
   isLoading: false,
   error: '',
   products: [],
+  currentProduct: null,
 }
 
 export const getProducts = createAsyncThunk('product/getProducts', async () => {
@@ -26,6 +28,18 @@ export const getProducts = createAsyncThunk('product/getProducts', async () => {
     })
 
     return productsData
+  } catch (err: any) {
+    throw Error(err)
+  }
+})
+
+export const addProduct = createAsyncThunk('product/addProduct', async () => {
+  try {
+    const productData: T_Product[] = await apiCall({
+      route: `/api/protected/product`,
+    })
+
+    return productData
   } catch (err: any) {
     throw Error(err)
   }
@@ -44,12 +58,26 @@ export const productSlice = createSlice({
       //---------------------------------------------------------------------
       .addCase(getProducts.pending, (state) => {
         state.isLoading = true
+        state.error = ''
       })
       .addCase(getProducts.fulfilled, (state, { payload }) => {
         state.isLoading = false
         state.products = payload
       })
       .addCase(getProducts.rejected, (state, { error }: AnyAction) => {
+        state.isLoading = false
+        state.error = error.message || ''
+      })
+      //---------------------------------------------------------------------
+      .addCase(addProduct.pending, (state) => {
+        state.isLoading = true
+        state.error = ''
+      })
+      .addCase(addProduct.fulfilled, (state, { payload }) => {
+        state.isLoading = false
+        state.products = payload
+      })
+      .addCase(addProduct.rejected, (state, { error }: AnyAction) => {
         state.isLoading = false
         state.error = error.message || ''
       })
