c4e6c95fb0a7303d32d6bde30f0008f39582aa9b
Matthew Powell <matt.powell2784@gmail.com>
2023-08-15 14:16:45 +0100

    feat: add bare client post route

--- src/app/api/protected/client/route.ts
+++ src/app/api/protected/client/route.ts
@@ -9,3 +9,20 @@ export const GET = async (_req: NextRequest, _res: NextResponse) => {
   const clients = await prisma.client.findMany()
   return NextResponse.json(clients, { status: 200 })
 }
+
+export const POST = async (req: NextRequest, _res: NextResponse) => {
+  const session = await getServerSession(authOptions)
+  if (!session) return noSessionResponse
+
+  const { name, add1, add2, postcode, tel } = await req.json()
+  console.log(
+    'name, add1, add2, postcode, tel',
+    name,
+    add1,
+    add2,
+    postcode,
+    tel
+  )
+
+  return NextResponse.json({ test: 'test' }, { status: 201 })
+}
