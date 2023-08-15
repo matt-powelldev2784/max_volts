9cc839c82ea43643bfdf0e3885da4a1e1515bcf9
Matthew Powell <matt.powell2784@gmail.com>
2023-08-15 16:24:30 +0100

    feat: add temp nav to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { NavBar } from '@/app/components'
+import { NavItems } from './components/navbar/components/NavItems'
 
 export default async function Home() {
   return (
     <main className="min-h-screen min-w-screen">
       <NavBar />
+      <div className="bg-darkBlack flex flex-col gap-8 p-4 h-fit min-h-screen">
+        <NavItems />
+      </div>
     </main>
   )
 }
