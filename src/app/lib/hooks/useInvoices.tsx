7de13bc7a784dceb305dd27644c413b620b45833
Matthew Powell <matt.powell2784@gmail.com>
2023-08-16 14:17:56 +0100

    fix: add types to useInvoices

--- src/app/lib/hooks/useInvoices.tsx
+++ src/app/lib/hooks/useInvoices.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxsHooks'
 import { getInvoices } from '@/redux/slice/newInvoiceSlice'
 
-export const useInvoices = (page) => {
+export const useInvoices = (page: string | number) => {
   const dispatch = useAppDispatch()
   const invoices = useAppSelector((state) => state.clientReducer.clients)
 
