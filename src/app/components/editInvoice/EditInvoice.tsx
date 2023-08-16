956261597fd6db6a8236d4c664034b30ae39af64
Matthew Powell <matt.powell2784@gmail.com>
2023-08-16 17:14:47 +0100

    feat: add client a select product fields to edit invoice

--- src/app/components/editInvoice/EditInvoice.tsx
+++ src/app/components/editInvoice/EditInvoice.tsx
@@ -2,6 +2,9 @@
 
 import { useInvoice } from '@/app/lib/hooks/useInvoice'
 import { useAppSelector } from '@/redux/hooks/reduxsHooks'
+import Image from 'next/image'
+import { AddProduct } from '../createInvoice/components/addProduct/AddProduct'
+import { ClientText } from './components/ClientText'
 
 interface EditInvoiceProps {
   invoiceId: string
@@ -12,7 +15,30 @@ export const EditInvoice = ({ invoiceId }: EditInvoiceProps) => {
   const invoice = useAppSelector(
     (state) => state.newInvoiceReducer.currentEditInvoice
   )
-  console.log('invoice', invoice)
 
-  return <div>Edit Invoice</div>
+  const invoiceNum = invoice?.invoiceNum
+
+  const clientText = invoice?.Client.companyName
+    ? `${invoice?.Client.name} @ ${invoice?.Client.companyName}`
+    : invoice?.Client.name
+
+  return (
+    <section>
+      <div className="flexRow gap-2 mt-4 mb-4">
+        <Image
+          src="/icons/invoice.svg"
+          alt="Person icon"
+          width={30}
+          height={30}
+          className=""
+        />
+        <p className="text-lg">Edit Invoice {invoiceNum}</p>
+      </div>
+
+      <div className="w-full flexRow p-2 md:px-12 lg:px-16 gap-4 lg:gap-16 flex-wrap lg:flex-nowrap mb-8">
+        <ClientText clientText={clientText} />
+        <AddProduct />
+      </div>
+    </section>
+  )
 }
---
+++ src/app/components/editInvoice/components/ClientText.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+interface ClientTextProps {
+  clientText: string | undefined
+}
+
+export const ClientText = ({ clientText }: ClientTextProps) => {
+  return (
+    <div className="w-full">
+      <div className="relative">
+        <div className="flexRow justify-end">
+          <Image
+            src="/icons/person.svg"
+            alt="arrow-down"
+            width={22}
+            height={22}
+            className="relative left-1"
+          />
+
+          <label className="relative left-1 w-full p-1 text-sm text-darkBlack/50">
+            Client
+          </label>
+        </div>
+        <p className="w-full rounded-lg border-2 bg-white p-2 px-4 outline-none pl-3 h-[42px] border-darkBlack/25 text-darkBlack/50">
+          {clientText ? clientText : 'Loading...'}
+        </p>
+      </div>
+    </div>
+  )
+}
