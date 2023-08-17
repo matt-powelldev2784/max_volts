e991ec849c532eada3f9ffb6ca834b643d1b1a09
Matthew Powell <matt.powell2784@gmail.com>
2023-08-17 16:57:15 +0100

    feat: add page title

--- src/app/components/invoiceMenu/InvoiceMenu.tsx
+++ src/app/components/invoiceMenu/InvoiceMenu.tsx
@@ -2,12 +2,24 @@
 
 import { Button } from '@/ui/button/button'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 export const InvoiceMenu = () => {
   const router = useRouter()
 
   return (
     <nav className="flexCol gap-4 m-4">
+      <div className="flexRow gap-2 mt-4 mb-5">
+        <Image
+          src="/icons/invoice.svg"
+          alt="Person icon"
+          width={30}
+          height={30}
+          className=""
+        />
+        <p className="text-lg">Invoice Menu</p>
+      </div>
+
       <Button
         type="button"
         optionalClasses="text-white text-sm bg-mvOrange w-full h-[42.5px] max-w-[320px]"
