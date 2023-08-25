6885728296c81af8efb00a48ee8770f4cd22295c
Matthew Powell <matt.powell2784@gmail.com>
2023-08-25 22:08:52 +0100

    feat: styles added to pdf invoice

--- src/app/components/invoice/components/pdf/PdfLayout.tsx
+++ src/app/components/invoice/components/pdf/PdfLayout.tsx
@@ -7,22 +7,43 @@ import {
   Image,
   Document,
   StyleSheet,
+  Font,
 } from '@react-pdf/renderer'
 import { T_Invoice } from '@/types/invoice'
+
+Font.register({
+  family: 'BrandonBold',
+  src: '/fonts/Brandon_bld.otf',
+})
+
+Font.register({
+  family: 'BrandonReg',
+  src: '/fonts/Brandon_reg.otf',
+})
 
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
+    fontFamily: 'BrandonReg',
+    fontSize: '12px',
   },
   logoSection: {
     flexDirection: 'column',
     alignItems: 'flex-end',
-    // border: '2px solid green',
   },
   imageContainer: {
     width: 200,
     height: 40,
     margin: 8,
+  },
+  flexCenter: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+  textBold: {
+    fontFamily: 'BrandonBold',
+    fontSize: '14px',
   },
 })
 
@@ -37,15 +58,23 @@ export const PdfLayout = ({ currentInvoice }: PdfLayoutProps) => {
       {/* eslint-disable jsx-a11y/alt-text */}
       <Page size="A4" style={styles.page}>
         <View style={styles.logoSection}>
-          <div style={styles.imageContainer}>
+          <View style={styles.imageContainer}>
             <Image src={`/max_volts_logo.jpg`} />
-          </div>
-          <Text>Max Volts Electrical Services</Text>
-          <Text>1 Road Name</Text>
-        </View>
+          </View>
 
-        <View style={styles.logoSection}>
-          <Text>Invoice Num: {currentInvoice.invoiceNum}</Text>
+          <View style={styles.flexCenter}>
+            <Text style={styles.textBold}>Invoice</Text>
+            <Text>Invoice Number: {currentInvoice.invoiceNum}</Text>
+          </View>
+
+          <View style={styles.flexCenter}>
+            <Text>
+              Max Volts Electrical Services, 1 Road Name, Worcester Park,
+              Surrey, KT11 1AA
+            </Text>
+            <Text>Tel: 07877 695 996</Text>
+            <Text>Email: max-volts-electrical@gmail.com</Text>
+          </View>
         </View>
       </Page>
       {/* eslint-enable jsx-a11y/alt-text */}
