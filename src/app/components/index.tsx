4af61ea9dfae6e876acf4ae2f5d20eb3786c4625
Matthew Powell <matt.powell2784@gmail.com>
2023-08-28 20:13:54 +0100

    fix: update edit product in component index v5

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -9,7 +9,7 @@ export { InvoiceList } from './invoice/InvoiceList'
 
 //product
 export { AddProduct } from './product/addProduct/AddProduct'
-export { EditProduct } from './product/editProducts/EditProduct'
+export { EditProduct } from './product/editProduct/EditProduct'
 
 //client
 export { AddClient } from './clients/AddClient'
