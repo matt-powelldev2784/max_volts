fdb854cd18c5f9ae02f3292e2238404e2c5aa436
Matthew Powell <matt.powell2784@gmail.com>
2023-08-28 20:02:23 +0100

    fix: update edit product in component index v2

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,11 +1,20 @@
-export { EditInvoice } from './invoice/EditInvoice'
-export { InvoiceList } from './invoice/InvoiceList'
-export { AddProduct } from './product/addProduct/AddProduct'
-export { EditProduct } from './product/editProduct/EditProduct'
+// common
 export { IsLoggedIn } from './IsLoggedIn/IsLoggedIn'
 export { NavBar } from './navbar/NavBar'
+
+//invoice
 export { CreateInvoice } from './invoice/CreateInvoice'
+export { EditInvoice } from './invoice/EditInvoice'
+export { InvoiceList } from './invoice/InvoiceList'
+
+//product
+export { AddProduct } from './product/addProduct/AddProduct'
+export { EditProduct } from './product/editProduct/EditProduct'
+
+//client
 export { AddClient } from './clients/AddClient'
+
+//menus
 export { InvoiceMenu } from './menuButtons/InvoiceMenu'
 export { ClientMenu } from './menuButtons/ClientMenu'
 export { ProductMenu } from './menuButtons/ProductMenu'
