23cb26210bca6931982d3e7a620d1f5850b081f0
Matthew Powell <matt.powell2784@gmail.com>
2023-08-28 11:11:40 +0100

    feat: add edit product skeleton

---
+++ src/app/components/product/editProduct/editProduct.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { InputField } from '@/app/lib/formElements/InputField'
+import { Button } from '@/ui/button/button'
+import { useEditProductFormik } from './lib/useEditProductFormik'
+import { PageTitle } from '@/app/lib/PageTitle'
+
+export const AddProduct = () => {
+  const formik = useEditProductFormik()
+
+  return (
+    <section className="min-h-screen w-screen">
+      <div className="flexCol mt-4">
+        <PageTitle text={'Add Product'} imgPath={'/icons/add_product.svg'} />
+
+        <form
+          className="w-screen md:w-[400px] flexCol min-w-[310px] p-2"
+          onSubmit={formik.handleSubmit}
+        >
+          <InputField
+            formik={formik}
+            htmlFor="name"
+            labelText="Name"
+            inputType="text"
+            imagePath="/icons/add_product.svg"
+            spanText=" *"
+          />
+
+          <InputField
+            formik={formik}
+            htmlFor="description"
+            labelText="Description"
+            inputType="text"
+            imagePath="/icons/description.svg"
+            spanText=" *"
+          />
+
+          <InputField
+            formik={formik}
+            htmlFor="buyPrice"
+            labelText="Buy Price"
+            inputType="number"
+            imagePath="/icons/pound_sign.svg"
+            spanText=" *"
+          />
+
+          <InputField
+            formik={formik}
+            htmlFor="sellPrice"
+            labelText="Sell Price"
+            inputType="number"
+            imagePath="/icons/pound_sign.svg"
+            spanText=" *"
+          />
+
+          <InputField
+            formik={formik}
+            htmlFor="VAT"
+            labelText="VAT"
+            inputType="number"
+            imagePath="/icons/percent_sign.svg"
+            spanText=" *"
+          />
+
+          <Button
+            type="submit"
+            optionalClasses="w-full text-white text-sm bg-mvOrange h-[42.5px] m-4"
+            buttonText="Add Product"
+            disabled={formik.isSubmitting}
+          />
+        </form>
+      </div>
+    </section>
+  )
+}
---
+++ src/app/components/product/editProduct/lib/useEditProductFormik.tsx
@@ -0,0 +1,53 @@
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { useAppDispatch } from '@/redux/hooks/reduxsHooks'
+import { addProduct } from '@/redux/slice/productSlice'
+import { T_ProductWithoutId } from '@/types'
+
+export const useEditProductFormik = () => {
+  const dispatch = useAppDispatch()
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      buyPrice: 0,
+      sellPrice: 0,
+      VAT: 20,
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Please input a name'),
+      description: Yup.string().required('Please input a description'),
+      buyPrice: Yup.number()
+        .typeError('Buy price must be a number')
+        .required('Please input buy price than must be a number')
+        .test(
+          'Is positive?',
+          'The number cannot be negative',
+          (value) => value > 0 || value === 0
+        ),
+      sellPrice: Yup.number()
+        .typeError('Sell price must be a number')
+        .required('Please input sell price than must be a number')
+        .test(
+          'Is positive?',
+          'The number cannot be negative',
+          (value) => value > 0 || value === 0
+        ),
+      VAT: Yup.number()
+        .typeError('Buy VAT must be a number')
+        .required('Please input VAT than must be a number')
+        .test(
+          'Is positive?',
+          'The number cannot be negative',
+          (value) => value > 0 || value === 0
+        ),
+    }),
+    onSubmit: async (values) => {
+      const newProduct: T_ProductWithoutId = values
+      dispatch(addProduct(newProduct))
+    },
+  })
+
+  return formik
+}
