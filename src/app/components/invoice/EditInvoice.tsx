b07b736b6d16888e092509dee8050719bf0fa9ce
Matthew Powell <matt.powell2784@gmail.com>
2023-08-30 19:29:55 +0100

    feat: update isPaid and isActive logic

--- src/app/components/invoice/EditInvoice.tsx
+++ src/app/components/invoice/EditInvoice.tsx
@@ -62,71 +62,73 @@ export const EditInvoice = ({ invoiceId }: EditInvoiceProps) => {
     router.push(`/pages/invoice/pdf/${invoiceId}`)
   }
 
-  return (
-    <section className="w-screen mt-4 mb-8">
-      <PageTitle
-        text={`Edit Invoice ${invoiceNum ? invoiceNum : ''}`}
-        imgPath={'/icons/invoice.svg'}
-        divClasses="mb-2"
-      />
+   console.log('isPaid----', isPaid)
+   console.log('isActive----', isActive)
+   return (
+     <section className="w-screen mt-4 mb-8">
+       <PageTitle
+         text={`Edit Invoice ${invoiceNum ? invoiceNum : ''}`}
+         imgPath={'/icons/invoice.svg'}
+         divClasses="mb-2"
+       />
 
-      {isPaid != undefined && isActive != undefined ? (
-        <InvoiceStatus isPaid={isPaid} isActive={isActive} />
-      ) : null}
+       <InvoiceStatus isPaid={isPaid} isActive={isActive} />
 
-      {isLoading ? <InvoiceIsLoading /> : null}
+       {isLoading ? <InvoiceIsLoading /> : null}
 
-      <div className="w-full flexCol">
-        <div className="flexCol w-full md:w-1/3">
-          {updateSuccess ? <ErrorMessage errorMessage={updateSuccess} /> : null}
-          {invoiceApiError ? <ErrorMessage /> : null}
-        </div>
-      </div>
+       <div className="w-full flexCol">
+         <div className="flexCol w-full md:w-1/3">
+           {updateSuccess ? (
+             <ErrorMessage errorMessage={updateSuccess} />
+           ) : null}
+           {invoiceApiError ? <ErrorMessage /> : null}
+         </div>
+       </div>
 
-      <div className="w-full flexCol mim-w-[305px] px-2 lg:px-4">
-        <div className="w-full flexCol md:p-2 gap-4 lg:flexRow md:px-12 lg:gap-16 mb-8">
-          <ClientText clientText={clientText} />
-          <AddProduct />
-        </div>
+       <div className="w-full flexCol mim-w-[305px] px-2 lg:px-4">
+         <div className="w-full flexCol md:p-2 gap-4 lg:flexRow md:px-12 lg:gap-16 mb-8">
+           <ClientText clientText={clientText} />
+           <AddProduct />
+         </div>
 
-        <div className="w-full lg:px:16 mim-w-[305px] md:p-2 ">
-          <InvoiceRowHeader />
-          {invoiceRowsJsx}
-        </div>
+         <div className="w-full lg:px:16 mim-w-[305px] md:p-2 ">
+           <InvoiceRowHeader />
+           {invoiceRowsJsx}
+         </div>
 
-        <p className="font-bold text-center mt-8 w-full mb-2">
-          Total Price : £{Number(totalPrice).toFixed(2)}
-        </p>
+         <p className="font-bold text-center mt-8 w-full mb-2">
+           Total Price : £{Number(totalPrice).toFixed(2)}
+         </p>
 
-        <Button
-          type="button"
-          optionalClasses={`text-white text-sm w-full h-[42.5px] max-w-[320px] ${
-            isLoading ? 'bg-mvOrange/50' : 'bg-mvOrange'
-          } `}
-          buttonText="Update Invoice"
-          disabled={isLoading}
-          onClick={onUpdateInvoiceClick}
-        />
+         <Button
+           type="button"
+           optionalClasses={`text-white text-sm w-full h-[42.5px] max-w-[320px] ${
+             isLoading ? 'bg-mvOrange/50' : 'bg-mvOrange'
+           } `}
+           buttonText="Update Invoice"
+           disabled={isLoading}
+           onClick={onUpdateInvoiceClick}
+         />
 
-        <div className="flexRow gap-2 mt-4">
-          <Button
-            type="button"
-            optionalClasses="text-white text-sm bg-mvOrange h-full w-[160px] max-h-[40px]"
-            buttonText={`Invoice Paid`}
-            onClick={() => router.push(`/pages/invoice/edit-invoice/`)}
-          />
-          <Button
-            type="button"
-            optionalClasses="text-white text-sm bg-mvOrange h-full w-[160px] max-h-[40px]"
-            buttonText={`Close Invoice`}
-            onClick={() => router.push(`/pages/invoice/edit-invoice/`)}
-          />
-        </div>
-      </div>
+         <div className="flexRow gap-2 mt-4">
+           <Button
+             type="button"
+             optionalClasses="text-white text-sm bg-mvOrange h-full w-[160px] max-h-[40px]"
+             buttonText={`Invoice Paid`}
+             onClick={() => router.push(`/pages/invoice/edit-invoice/`)}
+           />
+           <Button
+             type="button"
+             optionalClasses="text-white text-sm bg-mvOrange h-full w-[160px] max-h-[40px]"
+             buttonText={`Close Invoice`}
+             onClick={() => router.push(`/pages/invoice/edit-invoice/`)}
+           />
+         </div>
+       </div>
 
-      {showProductModal && currentInvoiceRow?.reduxId ? (
-        <InvoiceRowModal {...currentInvoiceRow} />
-      ) : null}
-    </section>
-  )
+       {showProductModal && currentInvoiceRow?.reduxId ? (
+         <InvoiceRowModal {...currentInvoiceRow} />
+       ) : null}
+     </section>
+   )
 }
--- src/app/components/invoice/components/invoiceStatus/InvoiceStatus.tsx
+++ src/app/components/invoice/components/invoiceStatus/InvoiceStatus.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Button } from '@/app/ui/button/button'
 
 interface InvoiceStatusProps {
-  isPaid: boolean
-  isActive: boolean
+  isPaid: boolean | undefined
+  isActive: boolean | undefined
 }
 
 export const InvoiceStatus = ({ isPaid, isActive }: InvoiceStatusProps) => {
+  if (isPaid && isActive === null) return <></>
+  if (isPaid === false && isActive === true) return <></>
+
   return (
     <div className="flexRow gap-2 mt-4">
       <Button
