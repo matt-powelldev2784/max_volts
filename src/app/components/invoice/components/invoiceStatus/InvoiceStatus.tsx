d9adf83b36c4c59857132078c4ed28407af4f3da
Matthew Powell <matt.powell2784@gmail.com>
2023-08-30 20:43:39 +0100

    fix: fix IsActiveText import v2

--- src/app/components/invoice/components/invoiceStatus/InvoiceStatus.tsx
+++ src/app/components/invoice/components/invoiceStatus/InvoiceStatus.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IsPaidText } from './isPaidText/IsPaidText'
+import { IsActiveText } from './isActiveText/IsActiveText'
 
 interface InvoiceStatusProps {
   isPaid: boolean | undefined
@@ -13,7 +14,7 @@ export const InvoiceStatus = ({ isPaid, isActive }: InvoiceStatusProps) => {
   return (
     <div className="flexRow gap-2 mt-4 mb-4">
       {isPaid ? <IsPaidText /> : null}
-      {/* {isActive ? null : <IsActiveText />} */}
+      {isActive ? null : <IsActiveText />}
     </div>
   )
 }
