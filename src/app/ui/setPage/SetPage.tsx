8ecdff1f511271f52dbae0c72f657c1e3ee72382
Matthew Powell <matt.powell2784@gmail.com>
2023-08-31 13:42:31 +0100

    feat: update opacity to show user loading state

--- src/app/ui/setPage/SetPage.tsx
+++ src/app/ui/setPage/SetPage.tsx
@@ -20,27 +20,28 @@ export const SetPage = ({
   const [isLoading, setIsLoading] = React.useState(false)
 
   const onClickNextRecords = () => {
-    if (currentPageNum + 1 <= maxPageNumber) {
+    if (currentPageNum + 1 <= maxPageNumber && !isLoading) {
       setIsLoading(true)
       router.push(`${baseUrl}${currentPageNum + 1}`)
     }
   }
 
   const onClickPrevRecords = () => {
-    if (currentPageNum - 1 >= 1) {
+    if (currentPageNum - 1 >= 1 && !isLoading) {
       setIsLoading(true)
       router.push(`${baseUrl}${currentPageNum - 1}`)
     }
   }
 
   return (
-    <div className="flexRow gap-4 m-2">
+    <div className="flexRow gap-8 m-2">
       <Image
         src="/icons/prev.svg"
         alt="Person icon"
         width={30}
         height={30}
         onClick={onClickPrevRecords}
+        className={`${isLoading ? 'opacity-50' : 'opacity-100'}`}
       />
 
       {isLoading ? <IsLoadingJsx /> : null}
@@ -51,6 +52,7 @@ export const SetPage = ({
         width={30}
         height={30}
         onClick={onClickNextRecords}
+        className={`${isLoading ? 'opacity-50' : 'opacity-100'}`}
       />
     </div>
   )
