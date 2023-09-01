7f38506d1efcbacb407da2d79fc48b3a7f140bcf
Matthew Powell <matt.powell2784@gmail.com>
2023-09-01 14:11:57 +0100

    refactor: remove button file from api folder

--- src/app/ui/index.ts
+++ src/app/ui/index.ts
@@ -1,4 +1,4 @@
-export { Button } from './../api/button/button'
+export { Button } from './/button/button'
 export { IsLoadingJsx } from './isLoadingJsx/IsLoadingJsx'
 export { PageTitle } from './pageTitle/PageTitle'
 export { SetPage } from './setPage/SetPage'
