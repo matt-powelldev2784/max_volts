c6e5e8fcefaf1ac73ea4188d472fd3eca05b1da5
Matthew Powell <matt.powell2784@gmail.com>
2023-09-02 08:51:42 +0100

    feat: add check email component

---
+++ src/app/components/auth/checkEmail/CheckEmail.tsx
@@ -0,0 +1,13 @@
+import { PageTitle } from '@/app/ui'
+
+export const CheckEmail = () => {
+  return (
+    <>
+      <PageTitle
+        text={'Email Verification Sent'}
+        imgPath={'/icons/email.svg'}
+        divClasses="mt-4 mb-8"
+      />
+    </>
+  )
+}
--- src/app/pages/auth/verify/page.tsx
+++ src/app/pages/auth/verify/page.tsx
@@ -1,5 +1,6 @@
 import { NavBar } from '@/app/components'
 import { getAuthProviders } from '../getProviders'
+import { CheckEmail } from '@/app/components'
 
 export default async function VerifyEmailPage() {
   const providers = await getAuthProviders()
@@ -7,6 +8,7 @@ export default async function VerifyEmailPage() {
   return (
     <>
       <NavBar />
+      <CheckEmail />
     </>
   )
 }
