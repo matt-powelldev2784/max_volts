66c003c3bc74bb40f2732e57a91a73f6269eaa00
Matthew Powell <matt.powell2784@gmail.com>
2023-09-02 11:03:26 +0100

    refactor: remove get providers from verify page

--- src/app/pages/auth/verify/page.tsx
+++ src/app/pages/auth/verify/page.tsx
@@ -1,10 +1,7 @@
 import { NavBar } from '@/app/components'
-import { getAuthProviders } from '../getProviders'
 import { CheckEmail } from '@/app/components'
 
 export default async function VerifyEmailPage() {
-  const providers = await getAuthProviders()
-  console.log('providers', providers)
   return (
     <>
       <NavBar />
