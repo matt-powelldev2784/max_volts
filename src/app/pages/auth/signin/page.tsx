c516284ed7e8ddf06427a698b68c6c72b885b45c
Matthew Powell <matt.powell2784@gmail.com>
2023-09-02 10:57:54 +0100

    feat: add auth error page

--- src/app/pages/auth/signin/page.tsx
+++ src/app/pages/auth/signin/page.tsx
@@ -3,20 +3,21 @@ import { getAuthProviders } from '../getProviders'
 import { SignIn } from '@/app/components'
 import { redirect } from 'next/navigation'
 
-export default async function SignInPage() {
-   const providers = await getAuthProviders()
 
-   return (
-     <>
-       <NavBar />
-       {providers ? (
-         <SignIn
-           oAuthProviders={providers.oAuthProviders}
-           emailProvider={providers.emailProvider}
-         />
-       ) : (
-         redirect('/pages/auth/error')
-       )}
-     </>
-   )
+export default async function SignInPage() {
+  const providers = await getAuthProviders()
+
+  return (
+    <>
+      <NavBar />
+      {providers ? (
+        <SignIn
+          oAuthProviders={providers.oAuthProviders}
+          emailProvider={providers.emailProvider}
+        />
+      ) : (
+        redirect('/pages/auth/error')
+      )}
+    </>
+  )
 }
