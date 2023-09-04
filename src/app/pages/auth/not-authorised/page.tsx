0396c24247829c8c3c9805eab17fb1965e9c864f
Matthew Powell <matt.powell2784@gmail.com>
2023-09-04 21:06:55 +0100

    feat: add not auth component and return to login logic

--- src/app/pages/auth/not-authorised/page.tsx
+++ src/app/pages/auth/not-authorised/page.tsx
@@ -1,21 +1,11 @@
 import { NavBar } from '@/app/components'
-import { PageTitle } from '@/app/ui'
+import { NotAuth } from '@/app/components'
 
 export default async function NotAuthorisedPage() {
   return (
     <>
       <NavBar />
-      <PageTitle
-        text={'Login Error'}
-        imgPath={'/icons/error.svg'}
-        divClasses="mt-4 mb-4"
-      />
-      <p className="text-center m-2">
-        You are not authorised to view this page.
-      </p>
-      <p className="text-center m-2">
-        Please contact the administrator to request access.
-      </p>
+      <NotAuth />
     </>
   )
 }
