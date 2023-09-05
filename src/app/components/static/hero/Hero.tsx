9e713cc87231f0f0b78121eb1c13ae44771296dd
Matthew Powell <matt.powell2784@gmail.com>
2023-09-05 15:24:13 +0100

    style: chnage text color

--- src/app/components/static/hero/Hero.tsx
+++ src/app/components/static/hero/Hero.tsx
@@ -18,7 +18,7 @@ export const Hero = () => {
         style={{ objectFit: 'cover' }}
       />
       <div className="absolute flexCol lg:items-start lg:left-32 lg:max-w-[800px] h-full w-full">
-        <p className="text-center lg:text-left font-bold text-xl md:text-3xl lg:text-5xl text-white mx-6 md:mx-16 lg:mx-0 bg-mvGreen/[0.9] rounded-xl p-4">
+        <p className="text-center lg:text-left font-bold text-xl md:text-3xl lg:text-5xl text-white mx-6 md:mx-16 lg:mx-0 bg-mvOrange/[0.9] rounded-xl p-4">
           {text}
         </p>
         <button className="bg-white px-4 py-2 rounded my-4 md:my-8 text-9xl">
