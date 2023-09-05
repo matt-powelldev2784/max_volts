c11d2d5ad78e9ef489995098c0848f9f5cecee9f
Matthew Powell <matt.powell2784@gmail.com>
2023-09-05 15:13:01 +0100

    feat: update text bg

--- src/app/components/static/hero/Hero.tsx
+++ src/app/components/static/hero/Hero.tsx
@@ -17,8 +17,8 @@ export const Hero = () => {
         fill
         style={{ objectFit: 'cover' }}
       />
-      <div className="absolute flexCol lg:items-start lg:left-32 lg:max-w-[800px] h-full w-full border-2 border-red-500">
-        <p className="text-center lg:text-left font-bold text-xl md:text-3xl lg:text-5xl text-mvOrange mx-6 md:mx-16 lg:mx-0 border-2 border-red-500">
+      <div className="absolute flexCol lg:items-start lg:left-32 lg:max-w-[800px] h-full w-full">
+        <p className="text-center lg:text-left font-bold text-xl md:text-3xl lg:text-5xl text-white mx-6 md:mx-16 lg:mx-0 bg-mvGreen/[0.9] rounded-xl p-4">
           {text}
         </p>
         <button className="bg-white px-4 py-2 rounded my-4 md:my-8 text-9xl">
