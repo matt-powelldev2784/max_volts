66fab7f0aa62f5f643286e2a7b5710977ea015d4
Matthew Powell <matt.powell2784@gmail.com>
2023-09-05 19:10:22 +0100

    feat: slide text updated

--- src/app/components/static/hero/data/slideData.ts
+++ src/app/components/static/hero/data/slideData.ts
@@ -1,21 +1,32 @@
+'use client'
+
 export const slideImages = [
   {
     key: 0,
     image: 'lights.jpg',
     alt: 'Photo of hanging lights',
-    text: 'Serving Commercial and Domestic Customers in Surrey and London.',
-  },
-  {
-    key: 1,
-    image: 'elec_man.jpg',
-    alt: 'Blue Boots',
-    text: 'The Electrical Contractor You Can Rely On',
+    title1: 'High',
+    title2: 'Quality',
+    title3: 'Local Electricians',
+    text: 'We offer a range of electrical services, from rewiring to installing sockets, switches, and lighting fixtures. We work with both commercial and domestic customers in Surrey and London. Call us now and see what we can do for you.',
   },
   {
     key: 2,
     image: 'elec_switch.jpg',
-    alt: 'Christiano Ronaldo',
-    text: 'Get A Free Quote Today',
+    alt: 'Electrical switch',
+    title1: 'Spark',
+    title2: 'Your',
+    title3: 'Imagination',
+    text: 'Max Volts is an electrical contractor that can turn your vision into reality. We offer creative and customised solutions for your electrical needs. Contact us today and let us show you what we can do.',
+  },
+  {
+    key: 2,
+    image: 'elec_man1.jpg',
+    alt: 'Electrician',
+    title1: 'Your',
+    title2: 'Trusted',
+    title3: 'Electrical Contractor',
+    text: 'Whether you need electrical services for your home or business, Max Volts is the contractor you can trust. Contact us today and get a free quote.',
   },
 ]
 
