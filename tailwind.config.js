27cc69f712493d4e2eb7d11d5d16f21ccff7cb37
Matthew Powell <matt.powell2784@gmail.com>
2023-09-06 18:42:47 +0100

    feat: update xl font lone spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
       sm: ['14px', '20px'],
       base: ['18px', '24px'],
       lg: ['22px', '28px'],
-      xl: ['26px', '30px'],
+      xl: ['26px', '32px'],
       '3xl': ['32px', '42px'],
       '5xl': ['52px', '64px'],
     },
