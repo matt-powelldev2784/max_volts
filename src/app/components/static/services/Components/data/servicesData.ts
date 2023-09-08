8b8da0cbc39cd06846c4e8a6a242f7b7d62e39ff
Matthew Powell <matt.powell2784@gmail.com>
2023-09-08 19:14:34 +0100

    feat: add servcies card data

---
+++ src/app/components/static/services/Components/data/servicesData.ts
@@ -0,0 +1,14 @@
+export const serviceData = [
+  {
+    title: 'Electrical Installations',
+    text: 'Quality electrical installations for residential and commercial properties. Trust our expert electricians for a reliable service.',
+  },
+  {
+    title: 'Electrical Repairs',
+    text: 'Efficient and skilled repairs for electrical issues. Our electricians ensure safety and satisfaction in every repair .',
+  },
+  {
+    title: 'Electrical Maintenance',
+    text: 'Comprehensive electrical maintenance services to keep your systems running smoothly. Trust our experienced electricians for regular upkeep.',
+  },
+]
