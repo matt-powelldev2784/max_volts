563378b39f2336f4bd3d946681ba4d6c02450e0a
Matthew Powell <matt.powell2784@gmail.com>
2023-09-10 19:50:07 +0100

    feat: repsonsive design updated

--- src/app/components/static/services/Components/cardItem/CardItem.tsx
+++ src/app/components/static/services/Components/cardItem/CardItem.tsx
@@ -7,8 +7,8 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ title, text, image }: CardItemProps) => (
-  <article className="w-fit sm:max-w-[520px] md:max-w-[800px] h-fit mx-auto flexCol md:flexRow lg:flexCol md:gap-8 overflow-hidden">
-    <div className="relative rounded-3xl overflow-hidden w-full md:max-w-[350px] z-20">
+  <article className="w-fit h-fit sm:max-w-[520px] md:max-w-[800px] mx-auto flexCol md:flexRow lg:flexCol md:gap-8 overflow-hidden">
+    <div className="relative rounded-3xl overflow-hidden w-full md:max-w-[350px] md:max-h-[205px] lg:max-h-[400px] z-20">
       <Image
         src={image}
         alt={'Electricial image'}
@@ -19,7 +19,7 @@ export const CardItem = ({ title, text, image }: CardItemProps) => (
     </div>
 
     <div className="relative w-full h-fit md:h-full z-10 mt-3 md:m-0 flex flex-col justify-between">
-      <div className="absolute -top-[116px] left-1/2 -translate-x-1/2 z-10 block md:hidden lg:block">
+      <div className="absolute -top-[116px] left-1/2 -translate-x-1/2 z-10 hidden lg:block">
         <Image
           src={'/services/spring.svg'}
           alt={'Electrician tools'}
@@ -29,7 +29,7 @@ export const CardItem = ({ title, text, image }: CardItemProps) => (
         />
       </div>
 
-      <div className="w-full h-[250px] md:max-w-[350px] lg:mt-4 flex flex-col justify-between">
+      <div className="w-full md:max-w-[350px] md:h-full lg:h-[250px] lg:mt-4 flex flex-col justify-between ">
         <h2 className="text-center text-mvGreen text-lg font-bold my-2 md:my-0 border-2 border-mvGreen rounded-xl p-2">
           {title}
         </h2>
@@ -42,7 +42,7 @@ export const CardItem = ({ title, text, image }: CardItemProps) => (
           type="button"
           className="text-white bg-mvOrange h-[45px] mt-4 md:mt-0 rounded-xl font-bold w-full"
         >
-          ENQUIRE NOW
+          MORE INFO
         </button>
       </div>
     </div>
