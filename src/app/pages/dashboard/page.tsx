554c47a0f0474203a440847105c59c96e4be6867
Matthew Powell <matt.powell2784@gmail.com>
2023-09-10 20:48:36 +0100

    feat: add dashboard page and set static site to homepage

---
+++ src/app/pages/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import { NavBar } from '@/app/components'
+import { ClientMenu, ProductMenu, InvoiceMenu } from '@/app/components'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions)
+  if (!session) return redirect('/api/auth/signin')
+  if (!session.user.isAdmin) return redirect('/pages/auth/not-authorised')
+
+  return (
+    <>
+      <NavBar />
+      <nav className="flex flex-row w-full justify-center items-start flex-wrap gap-2 md:mt-4">
+        <InvoiceMenu />
+        <ClientMenu />
+        <ProductMenu />
+      </nav>
+    </>
+  )
+}
