d39f83d90ed132777d9432d85d97249fcf91bfb0
Matthew Powell <matt.powell2784@gmail.com>
2023-09-10 19:54:23 +0100

    feat: repsonsive design updated

--- src/app/components/static/services/Services.tsx
+++ src/app/components/static/services/Services.tsx
@@ -12,11 +12,11 @@ export const Services = () => {
   ))
 
   return (
-    <section className="bg-darkBlack py-8 lg:py-16 px-8">
+    <section className="bg-darkBlack py-8 lg:py-16 px-8 pb-16">
       <h1 className="w-full text-center text-xl lg:text-2xl font-bold text-mvOrange mb-4 md:mb-8">
         OUR SERVICES
       </h1>
-      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 gap-16 md:gap-20 lg:gap-20 place-content-center justify-center">
+      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 gap-20 lg:gap-20 place-content-center justify-center">
         {servicesCardsJsx}
       </div>
     </section>
