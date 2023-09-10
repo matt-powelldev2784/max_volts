a537c7709997decd96484003537756293d4e149a
Matthew Powell <matt.powell2784@gmail.com>
2023-09-10 19:25:44 +0100

    feat: repsonsive design updated

--- src/app/components/static/services/Components/cardItem/CardItem.tsx
+++ src/app/components/static/services/Components/cardItem/CardItem.tsx
@@ -7,8 +7,8 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ title, text, image }: CardItemProps) => (
-  <article className="w-fit max-w-[520px] h-fit mx-auto flexCol md:flexRow lg:flexCol md:gap-8 overflow-hidden border-2 border-green-500">
-    <div className="relative rounded-3xl overflow-hidden w-full md:max-w-[350px] z-20 border-2 border-red-500 ">
+  <article className="w-fit sm:max-w-[520px] md:max-w-[800px] h-fit mx-auto flexCol md:flexRow lg:flexCol md:gap-8 overflow-hidden">
+    <div className="relative rounded-3xl overflow-hidden w-full md:max-w-[350px] z-20">
       <Image
         src={image}
         alt={'Electricial image'}
@@ -18,8 +18,8 @@ export const CardItem = ({ title, text, image }: CardItemProps) => (
       />
     </div>
 
-    <div className="relative w-full h-fit md:h-full z-10 mt-3 md:m-0 flex flex-col justify-between ">
-      <div className="absolute -top-[116px] left-1/2 -translate-x-1/2 z-10 block md:hidden lg:block ">
+    <div className="relative w-full h-fit md:h-full z-10 mt-3 md:m-0 flex flex-col justify-between">
+      <div className="absolute -top-[116px] left-1/2 -translate-x-1/2 z-10 block md:hidden lg:block">
         <Image
           src={'/services/spring.svg'}
           alt={'Electrician tools'}
@@ -29,12 +29,12 @@ export const CardItem = ({ title, text, image }: CardItemProps) => (
         />
       </div>
 
-      <div className="w-full h-[250px] md:max-w-[350px] lg:mt-4 flex flex-col justify-between border-2 border-blue-500">
+      <div className="w-full h-[250px] md:max-w-[350px] lg:mt-4 flex flex-col justify-between">
         <h2 className="text-center text-mvGreen text-lg font-bold my-2 md:my-0 border-2 border-mvGreen rounded-xl p-2">
           {title}
         </h2>
 
-        <p className="grow text-justify text-white md:text-center lg:text-justify my-4 border-2 border-red-500">
+        <p className="grow text-justify text-white md:text-center lg:text-justify my-4">
           {text}
         </p>
 
--- src/app/components/static/services/Services.tsx
+++ src/app/components/static/services/Services.tsx
@@ -16,7 +16,7 @@ export const Services = () => {
       <h1 className="w-full text-center text-xl lg:text-2xl font-bold text-mvOrange mb-2 md:mb-8">
         OUR SERVICES
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-12 mx-4 place-content-center justify-center">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-20 mx-4 place-content-center justify-center">
         {servicesCardsJsx}
       </div>
     </section>
