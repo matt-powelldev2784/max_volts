9ed0f33e138cea82b50cc4c82d7edb7ae025dfc0
Matthew Powell <matt.powell2784@gmail.com>
2023-09-19 18:50:01 +0100

    feat: only return products where is hideen is false

--- src/app/pages/product/product-list/getTenProducts.ts
+++ src/app/pages/product/product-list/getTenProducts.ts
@@ -5,6 +5,7 @@ export const getTenProducts = async (pageNumber: number) => {
   const skip = (pageNumber - 1) * pageSize
 
   const products = await prisma.product.findMany({
+    where: { isHidden: false },
     orderBy: { name: 'asc' },
     skip,
     take: pageSize,
