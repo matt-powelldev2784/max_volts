a23a4a47d6afde41ea7c378ba31b43c26a63e220
Matthew Powell <matt.powell2784@gmail.com>
2023-09-19 18:46:30 +0100

    feat: add remove product ui and logic

--- src/app/components/product/editProduct/EditProduct.tsx
+++ src/app/components/product/editProduct/EditProduct.tsx
@@ -1,17 +1,30 @@
 'use client'
 
+import { useState } from 'react'
 import { InputField } from '@/app/ui/formElements/InputField'
 import { useEditProductFormik } from './lib/useEditProductFormik'
 import { PageTitle, Button } from '@/app/ui/'
 import { T_Product } from '@/types'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks/reduxsHooks'
+import { setProductIsHidden } from '@/redux/slice/productSlice'
 
 interface EditProductProps {
   product: T_Product
 }
 
 export const EditProduct = ({ product }: EditProductProps) => {
+  const [confirmDelete, setConfirmDelete] = useState<boolean>(false)
+  const dispatch = useAppDispatch()
   const formik = useEditProductFormik(product)
-  console.log('formik.isSubmitting', formik.isSubmitting)
+
+  const onDeleteClientClick = async () => {
+    if (product.id) {
+      await dispatch(setProductIsHidden(product.id))
+      window.location.href = `/pages/product/product-list/1`
+    }
+  }
+
+  const isLoading = useAppSelector((state) => state.productReducer.isLoading)
 
   return (
     <section className="min-h-screen w-screen">
@@ -76,6 +89,30 @@ export const EditProduct = ({ product }: EditProductProps) => {
             disabled={formik.isSubmitting}
           />
         </form>
+
+        <div className="w-screen md:w-[400px] flexCol min-w-[310px] p-2 mt-4">
+          {confirmDelete ? (
+            <Button
+              type="button"
+              optionalClasses={`text-white text-sm h-full w-full max-h-[42.5px] ${
+                isLoading ? 'bg-darkRed/25' : 'bg-darkRed'
+              }`}
+              buttonText="Confirm Delete Product"
+              disabled={isLoading === true}
+              onClick={onDeleteClientClick}
+            />
+          ) : (
+            <Button
+              type="button"
+              optionalClasses={`text-white text-sm bg-darkBlack h-full w-fit max-h-[42.5px]`}
+              buttonText="Delete Product"
+              disabled={isLoading === true}
+              onClick={() => {
+                setConfirmDelete(true)
+              }}
+            />
+          )}
+        </div>
       </div>
     </section>
   )
