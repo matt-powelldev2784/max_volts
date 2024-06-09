edbd79d581c39f4c97eff267bb1d030caf8f38b5
Matthew Powell <matt.powell2784@gmail.com>
2024-06-09 07:53:02 +0100

    feat: update invoice type

--- src/types/invoice.ts
+++ src/types/invoice.ts
@@ -27,7 +27,7 @@ export interface T_UpdateInvoiceDetails {
 export interface T_Invoice {
   id: string
   invoiceNum: number
-  quoteNum?: number
+  quoteNum?: number | null
   clientId: string | Date
   invoiceDate: Date
   totalAmount: number
